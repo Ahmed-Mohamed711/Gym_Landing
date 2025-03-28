ba80ba4ae3b2316afbd6ad8c31f785d3af6d933e
Ahmed-Mohamed711 <ahmed.mohamed24711@gmail.com>
2025-03-28 14:14:26 +0200

    upload3

--- src/components/navbar/Navbar3.jsx
+++ src/components/navbar/Navbar3.jsx
@@ -1,8 +1,94 @@
+// import "./navbar3.css";
+// import Logo from "../../images3/logo.png";
+// import Bars from "../../images3/bars.png";
+// import { Link } from "react-scroll";
+// import { useState, useEffect } from "react";
+// const Navbar3 = () => {
+//   const [menuOpened, setMenuOpened] = useState(false);
+//   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+//   useEffect(() => {
+//     const handleResize = () => setIsMobile(window.innerWidth <= 768);
+//     window.addEventListener("resize", handleResize);
+//     return () => window.removeEventListener("resize", handleResize);
+//   }, []);
+
+//   return (
+//     <div className="navbar3">
+//       <img src={Logo} alt="" className="logo" />
+
+//       {isMobile && !menuOpened ? (
+//         <div
+//           style={{
+//             backgroundColor: "gray",
+//             padding: "0.5rem",
+//             borderRadius: "5px",
+//             cursor: "pointer",
+//           }}
+//           onClick={() => setMenuOpened(true)}
+//         >
+//           <img
+//             src={Bars}
+//             alt=""
+//             style={{ width: "1.5rem", height: "1.5rem" }}
+//           />
+//         </div>
+//       ) : (
+//         <ul className="navbar3-menu">
+//           <li>
+//             <Link onClick={() => setMenuOpened(false)} to="hero3" smooth={true}>
+//               Home
+//             </Link>
+//           </li>
+//           <li>
+//             <Link
+//               onClick={() => setMenuOpened(false)}
+//               to="Project3"
+//               smooth={true}
+//             >
+//               Programs
+//             </Link>
+//           </li>
+//           <li>
+//             <Link
+//               onClick={() => setMenuOpened(false)}
+//               to="Reasons3"
+//               smooth={true}
+//             >
+//               Why us
+//             </Link>
+//           </li>
+//           <li>
+//             <Link
+//               onClick={() => setMenuOpened(false)}
+//               to="plans3"
+//               smooth={true}
+//             >
+//               Plans
+//             </Link>
+//           </li>
+//           <li>
+//             <Link
+//               onClick={() => setMenuOpened(false)}
+//               to="testimonials3"
+//               smooth={true}
+//             >
+//               Testimonials
+//             </Link>
+//           </li>
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Navbar3;
 import "./navbar3.css";
 import Logo from "../../images3/logo.png";
 import Bars from "../../images3/bars.png";
 import { Link } from "react-scroll";
 import { useState, useEffect } from "react";
+
 const Navbar3 = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
@@ -14,27 +100,16 @@ const Navbar3 = () => {
   }, []);
 
   return (
-    <div className="navbar3">
-      <img src={Logo} alt="" className="logo" />
+    <nav className="navbar3">
+      <img src={Logo} alt="Logo" className="logo" />
 
+      {/* أيقونة القائمة في وضع الجوال */}
       {isMobile && !menuOpened ? (
-        <div
-          style={{
-            backgroundColor: "gray",
-            padding: "0.5rem",
-            borderRadius: "5px",
-            cursor: "pointer",
-          }}
-          onClick={() => setMenuOpened(true)}
-        >
-          <img
-            src={Bars}
-            alt=""
-            style={{ width: "1.5rem", height: "1.5rem" }}
-          />
+        <div className="menu-icon" onClick={() => setMenuOpened(true)}>
+          <img src={Bars} alt="Menu" className="menu-bars" />
         </div>
       ) : (
-        <ul className="navbar3-menu">
+        <ul className={`navbar3-menu ${isMobile ? "mobile-menu" : ""}`}>
           <li>
             <Link onClick={() => setMenuOpened(false)} to="hero3" smooth={true}>
               Home
@@ -78,7 +153,7 @@ const Navbar3 = () => {
           </li>
         </ul>
       )}
-    </div>
+    </nav>
   );
 };
 
