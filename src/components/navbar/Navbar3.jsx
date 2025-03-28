c7307a22c15a243c04a0ada38c47d0a0becf160d
Ahmed-Mohamed711 <ahmed.mohamed24711@gmail.com>
2025-03-28 14:00:39 +0200

    upload2

--- src/components/navbar/Navbar3.jsx
+++ src/components/navbar/Navbar3.jsx
@@ -1,68 +1,85 @@
-import React, { useState } from 'react'
-import "./navbar3.css"
+import "./navbar3.css";
 import Logo from "../../images3/logo.png";
 import Bars from "../../images3/bars.png";
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
+import { useState, useEffect } from "react";
 const Navbar3 = () => {
-  const mobile = window.innerWidth<=768?true:false;
-  const [menuOpend,setMenuOpend]=useState(false);
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className='navbar3'>
-        <img src={Logo} alt='' className='logo'/> 
-        {(menuOpend=== false && mobile===true)?(
-          <div
-          style={{backgroundColor:'gray',
-          padding:'0.5rem',
-          borderRadius:'5px',
-          cursor:'pointer'
-        }}
+    <div className="navbar3">
+      <img src={Logo} alt="" className="logo" />
 
-          onClick={()=> setMenuOpend(true)}
-          >
-            <img src={Bars} alt='' style={{width:"1.5rem" ,height:'1.5rem'}}/></div>
-        ): 
-        (<ul className='navbar3-menu'>
+      {isMobile && !menuOpened ? (
+        <div
+          style={{
+            backgroundColor: "gray",
+            padding: "0.5rem",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+          onClick={() => setMenuOpened(true)}
+        >
+          <img
+            src={Bars}
+            alt=""
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
+        </div>
+      ) : (
+        <ul className="navbar3-menu">
+          <li>
+            <Link onClick={() => setMenuOpened(false)} to="hero3" smooth={true}>
+              Home
+            </Link>
+          </li>
           <li>
             <Link
-            onClick={()=>setMenuOpend(false)}
-            to='hero3'
-            span={true}
-            smooth={true}
-            >Home</Link>
-            </li>
-
-          <li >
-            <Link
-            onClick={()=>setMenuOpend(false)}
-            to='Project3'
-            span={true}
-            smooth={true}
-            >Programs</Link></li>
-          <li >
-            <Link
-            onClick={()=>setMenuOpend(false)}
-            to='Reasons3'
-            span={true}
-            smooth={true}
-          >Why us</Link></li>
+              onClick={() => setMenuOpened(false)}
+              to="Project3"
+              smooth={true}
+            >
+              Programs
+            </Link>
+          </li>
           <li>
             <Link
-            onClick={()=>setMenuOpend(false)}
-            to='plans3'
-            span={true}
-            smooth={true}
-            >Plans</Link></li>
-          <li><Link
-          onClick={()=>setMenuOpend(false)}
-          to='testimonials3'
-          span={true}
-          smooth={true}
-          >Testimonials</Link></li>
-       </ul>
-        )}
-        
+              onClick={() => setMenuOpened(false)}
+              to="Reasons3"
+              smooth={true}
+            >
+              Why us
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="plans3"
+              smooth={true}
+            >
+              Plans
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="testimonials3"
+              smooth={true}
+            >
+              Testimonials
+            </Link>
+          </li>
+        </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar3
+export default Navbar3;
